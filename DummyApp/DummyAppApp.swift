002842681895f158001c7f2e5649acfd66c56017
timcop <timothy.copland98@gmail.com>
2021-08-12 17:41:54 +1200

    Initial Commit

---
+++ DummyApp/DummyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyAppApp.swift
+//  DummyApp
+//
+//  Created by Tim Copland on 12/08/21.
+//
+
+import SwiftUI
+
+@main
+struct DummyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
